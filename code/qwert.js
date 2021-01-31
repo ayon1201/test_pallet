11f94a7a49194933ab1cff772beb9169bd34f70d
Abha Jha <jhaabha08@gmail.com>
2021-01-31 20:21:01 +0530

    [ADD] save,save and next

--- code/qwert.js
+++ code/qwert.js
@@ -3,26 +3,29 @@ let QnA=[
         id:"1",
         question:"Question 1?",
         answers:[
-            {text:"Answer 1", correct:true},
-            {text:"Answer 2", correct:false}
-        ]
+            {id:1, text:"Answer 1", correct:true},
+            {id:2, text:"Answer 2", correct:false}
+        ],
+        answer_selected:-1
     },
     {
         id:"2",
         question:"Question 2?",
         answers:[
-            {text:"Answer 1", correct:true},
-            {text:"Answer 2", correct:false}
-        ]
+            {id:1, text:"Answer 1", correct:true},
+            {id:2, text:"Answer 2", correct:false}
+        ],
+        answer_selected:-1
     },
     {
         id:"2",
         question:"Question 3?",
         answers:[
-            {text:"Answer 1", correct:false},
-            {text:"Answer 2", correct:true},
-            {text:"Answer 3", correct:false}
-        ]
+            {id:1, text:"Answer 1", correct:false},
+            {id:2, text:"Answer 2", correct:true},
+            {id:3, text:"Answer 3", correct:false}
+        ],
+        answer_selected:-1
     }
 ]
 
@@ -32,16 +35,34 @@ window.onload=()=>{
 
     let nextButton=document.getElementById("btnNextQue");
     let backButton=document.getElementById("btnPrevQue");
+    let QuestionPalette=document.getElementById("test-questions");
+    let saveBtn=document.getElementById("saveAndNext")
+    let paletteHtml=""
+    let selectedAnswer=-1;
+
+    for(i=1;i<=90;i++){
+        paletteHtml+=`<li class="" data-seq="1"><a id="question-no${i}" class="test-ques que-not-attempted"
+        href="javascript:void(0);" data-href="page${i}">${i}</a></li>`
+    }
+
+    QuestionPalette.innerHTML=paletteHtml;
+
+    let selectAnswer=(answer_no)=>{
+        selectedAnswer=answer_no;
+    }
 
     displayQuestion=()=>{
+        selectedAnswer=-1;
         document.getElementById("question-text").innerHTML=QnA[q_no].question;
 
         ansHtml=""
         QnA[q_no].answers.forEach((answer,id)=>{
-            ansHtml += `<td><input type="radio" name="radiospage01" value=${id}>${answer.text}<br>`
+            ansHtml += `<td><input id=${id+1} type="radio" name="radiospage01" value=${id+1}>${answer.text}<br>`
         })
-
         document.getElementById('answer-options').innerHTML = ansHtml;
+        QnA[q_no].answers.forEach((answer,id)=>{
+            document.getElementById(`${id+1}`).addEventListener('click',()=>selectAnswer(id+1))
+        })
         
         if(q_no==0)
         backButton.style.display='none';
@@ -64,9 +85,21 @@ window.onload=()=>{
         displayQuestion();
     }
 
+    save=()=>{
+        QnA[q_no].answer_selected=selectedAnswer;
+    }
+
+    saveAndNext=()=>{
+        save()
+        if(QnA[q_no].answer_selected!=-1)
+        document.getElementById(`question-no${q_no+1}`).style.backgroundColor="green";
+        nextQuestion()
+    }
+
     displayQuestion();
-    nextButton.addEventListener('click',nextQuestion);
-    backButton.addEventListener('click',prevQuestion);
+    nextButton.addEventListener('click',nextQuestion)
+    backButton.addEventListener('click',prevQuestion)
+    saveBtn.addEventListener('click',saveAndNext)
 }
 
 
